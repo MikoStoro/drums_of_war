e13065ef6cc7ccf9e59a6bde25614cc21d02cf10
Mikołaj Storoniak <miko.storo@gmail.com>
2025-03-21 13:24:16 +0100

    actions can be changed dynamically

---
+++ move_action.gd
@@ -0,0 +1,20 @@
+extends Node
+
+var action_name = "Move Right"
+
+const SPEED = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func perform_action(character):
+	character.dir = 1
+	character.velocity.x = character.dir * SPEED
+
+func end_action(character):
+	character.velocity.x = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
