18e7ab51fb3a68b1e66002bac462d5421ddff4ee
Executed419 <lucker10@interia.pl>
2025-05-05 19:28:12 +0200

    movement visualized

--- Scripts/visual_player.gd
+++ Scripts/visual_player.gd
@@ -13,17 +13,21 @@ var inputs = {"ui_right": Vector2.RIGHT,
 func new_orders(arr: Array[BoardEvent]) -> void:
 	for event: BoardEvent in arr:
 		if event.type == GlobalEnums.event_type.MOVE:
-			#move(event)
-			print("where coords")
+			move(event.place)
 
 func _process(delta: float) -> void:
 	pass
 
-	# only for animating, no damage dealt here
 	# if I understand correctly, AnimationPlayer for using keyframes 
 	# and AnimationSprite for spritesheets
+	
+func _draw_line(arr: Array[Vector2]):
+	draw_polyline(arr, Color.REBECCA_PURPLE)
+
 func attack():
 	animation_sprite.play("attack")
+	var array = PackedVector2Array([Vector2(12, 34), Vector2(56, 78)])
+	_draw_line(array)
 
 func play_animation(name: String) -> void:
 	# not sure how to properly corelate speed in code with animation player
@@ -31,20 +35,20 @@ func play_animation(name: String) -> void:
 	animation.play(name)
 	
 	
-func move(x: int, y: int) -> void:
+func move(coords: Vector2) -> void:
 	play_animation("move")
-	var coords = Vector2(x,y) 
+	coords = Vector2(coords.y,coords.x) 
 	var new_position = coords * tile_size
 	var tween = create_tween()
 	tween.tween_property(self, "position", new_position, 0.2) 
 	
 	
-func _unhandled_input(event):
-	for dir in inputs.keys():
-		if event.is_action_pressed(dir):
-			move(3,2)
-	if event.is_action_pressed("player1_action1"):
-			move(2,1)
-			attack()
+#func _unhandled_input(event):
+	#for dir in inputs.keys():
+		#if event.is_action_pressed(dir):
+			#move(Vector2(3,2))
+	#if event.is_action_pressed("player1_action1"):
+			#move(Vector2(2,1))
+			#attack()
 	
 	
