01283eefd6145097d12a4483746fc36e7cc4985a
Executed419 <lucker10@interia.pl>
2025-05-06 09:44:37 +0200

    testing different options to draw line

--- Scripts/visual_player.gd
+++ Scripts/visual_player.gd
@@ -9,25 +9,49 @@ var inputs = {"ui_right": Vector2.RIGHT,
 			"ui_up": Vector2.UP,
 			"ui_down": Vector2.DOWN}
 			
+var debug_points=PackedVector2Array([
+	Vector2(0,0),
+	Vector2(1000,1000),
+	Vector2(100,000)
+])
 		
 func new_orders(arr: Array[BoardEvent]) -> void:
 	for event: BoardEvent in arr:
 		if event.type == GlobalEnums.event_type.MOVE:
 			move(event.place)
+		if event.type == GlobalEnums.event_type.ATTACK:
+			_draw_line(PackedVector2Array([event.place * tile_size, position * tile_size]))
 
 func _process(delta: float) -> void:
 	pass
 
 	# if I understand correctly, AnimationPlayer for using keyframes 
 	# and AnimationSprite for spritesheets
+func float_array_to_Vector2Array(coords : Array) -> PackedVector2Array:
+	# Convert the array of floats into a PackedVector2Array.
+	var array : PackedVector2Array = []
+	for coord in coords:
+		array.append(Vector2(coord[0], coord[1]))
+	return array
 	
-func _draw_line(arr: Array[Vector2]):
-	draw_polyline(arr, Color.REBECCA_PURPLE)
-
-func attack():
+func _draw_line(arr: PackedVector2Array):
+	#queue_redraw()
+	var line := Line2D.new()
+	add_child(line)
+	line.add_point(position)
+	line.add_point(Vector2(100,100)) 
+	line.remove_point(0)
+	#draw_colored_polygon(debug_points, Color.RED)
+	#
+#func _draw():
+	#if Input.is_action_pressed("ui_down"):
+		#draw_colored_polygon(debug_points, Color.RED)
+		
+func attack(arr: Array[Vector2]):
 	animation_sprite.play("attack")
-	var array = PackedVector2Array([Vector2(12, 34), Vector2(56, 78)])
-	_draw_line(array)
+	#var array = PackedVector2Array([Vector2(12, 34), Vector2(56, 78)])
+	_draw_line(float_array_to_Vector2Array(arr))
+	#_draw_line(100,100)
 
 func play_animation(name: String) -> void:
 	# not sure how to properly corelate speed in code with animation player
