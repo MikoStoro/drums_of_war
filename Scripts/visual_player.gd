a4cd0d5e1228018c24f48466ead0cba50a443756
Executed419 <lucker10@interia.pl>
2025-05-06 16:01:12 +0200

    Chicken Jockey!

--- Scripts/visual_player.gd
+++ Scripts/visual_player.gd
@@ -3,56 +3,37 @@ extends Node2D
 @export var animation: AnimationPlayer
 @export var animation_sprite: AnimatedSprite2D
 # set this somewhere else (maybe when creating player)
-var tile_size = 16
+var tile_size := 16.0
 var inputs = {"ui_right": Vector2.RIGHT,
 			"ui_left": Vector2.LEFT,
 			"ui_up": Vector2.UP,
 			"ui_down": Vector2.DOWN}
-			
-var debug_points=PackedVector2Array([
-	Vector2(0,0),
-	Vector2(1000,1000),
-	Vector2(100,000)
-])
 		
 func new_orders(arr: Array[BoardEvent]) -> void:
 	for event: BoardEvent in arr:
-		if event.type == GlobalEnums.event_type.MOVE:
-			move(event.place)
-		if event.type == GlobalEnums.event_type.ATTACK:
-			_draw_line(PackedVector2Array([event.place * tile_size, position * tile_size]))
-
-func _process(delta: float) -> void:
-	pass
-
-	# if I understand correctly, AnimationPlayer for using keyframes 
-	# and AnimationSprite for spritesheets
-func float_array_to_Vector2Array(coords : Array) -> PackedVector2Array:
-	# Convert the array of floats into a PackedVector2Array.
-	var array : PackedVector2Array = []
-	for coord in coords:
-		array.append(Vector2(coord[0], coord[1]))
-	return array
-	
-func _draw_line(arr: PackedVector2Array):
-	#queue_redraw()
-	var line := Line2D.new()
-	add_child(line)
-	line.add_point(position)
-	line.add_point(Vector2(100,100)) 
-	line.remove_point(0)
-	#draw_colored_polygon(debug_points, Color.RED)
-	#
-#func _draw():
-	#if Input.is_action_pressed("ui_down"):
-		#draw_colored_polygon(debug_points, Color.RED)
+		var newArr: Array[Vector2] = []
+		for e in event.place:
+			var v := Vector2(e.y * tile_size - tile_size/2, e.x * tile_size - tile_size/2)
+			newArr.append(v)
+		event.place = newArr
 		
-func attack(arr: Array[Vector2]):
-	animation_sprite.play("attack")
-	#var array = PackedVector2Array([Vector2(12, 34), Vector2(56, 78)])
-	_draw_line(float_array_to_Vector2Array(arr))
-	#_draw_line(100,100)
+		if event.type == GlobalEnums.event_type.MOVE:
+			move(event.place[0])
+		if event.type == GlobalEnums.event_type.ATTACK:
+			_draw_line(event.place)
 
+func _draw_line(arr: Array[Vector2]):
+	var line := Line2D.new()
+	get_tree().root.add_child(line)
+	line.width = 3
+	line.default_color = Color.CRIMSON
+	
+	for pos in arr:
+		line.add_point(pos)
+		
+	await get_tree().create_timer(1.0).timeout
+	line.queue_free()
+		
 func play_animation(name: String) -> void:
 	# not sure how to properly corelate speed in code with animation player
 	animation.speed_scale = 5
@@ -61,18 +42,11 @@ func play_animation(name: String) -> void:
 	
 func move(coords: Vector2) -> void:
 	play_animation("move")
-	coords = Vector2(coords.y,coords.x) 
-	var new_position = coords * tile_size
+	#coords = Vector2(coords.y,coords.x) 
+	var new_position = coords
 	var tween = create_tween()
 	tween.tween_property(self, "position", new_position, 0.2) 
 	
 	
-#func _unhandled_input(event):
-	#for dir in inputs.keys():
-		#if event.is_action_pressed(dir):
-			#move(Vector2(3,2))
-	#if event.is_action_pressed("player1_action1"):
-			#move(Vector2(2,1))
-			#attack()
 	
 	
