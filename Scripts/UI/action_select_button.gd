e3744afbb76fa00d01df60016be787d6dd161fda
Executed419 <lucker10@interia.pl>
2025-05-30 12:40:30 +0200

    ui action display fix

--- Scripts/UI/action_select_button.gd
+++ Scripts/UI/action_select_button.gd
@@ -3,10 +3,14 @@ extends Node
 var actions_dict=[]
 
 func _ready() -> void:
+	var i = 0	
 	for child:OptionButton in get_children():
 		for action:BaseAction in Character_Manager.character.actions:
 			child.add_item(action.action_name)
 			actions_dict.append(action)
+			if Character_Manager.character.actions[i] == action:
+				child.selected = i
+		i+=1
 
 
 func _on_action_item_selected(index: int) -> void:
