965849225159ddd857ca7c80e97db1a9da489cd6
Executed419 <lucker10@interia.pl>
2025-10-01 22:47:47 +0200

    spawn at coords

--- Scripts/UI/button_spawn.gd
+++ Scripts/UI/button_spawn.gd
@@ -1,6 +1,7 @@
 extends Button
-
+@export var x:TextEdit
+@export var y:TextEdit
 
 func _on_button_down() -> void:
 	var z= get_tree().get_first_node_in_group("CharacterManager")
-	z.add_character(1,1)
+	z.add_character(y.text.to_int(),x.text.to_int())
