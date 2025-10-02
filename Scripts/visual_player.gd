a2845659f696628912ffe16db3801b39168343b9
Executed419 <lucker10@interia.pl>
2025-10-02 11:54:26 +0200

    dir from gamepad

--- Scripts/visual_player.gd
+++ Scripts/visual_player.gd
@@ -6,6 +6,7 @@ class_name VisualCharacter
 @export var player_animations: AnimatedSprite2D
 #@export var line_texture: Texture
 # set this somewhere else (maybe when creating player)
+var deadzone := 0.2
 var tile_size : float = VisualBoardTools.tile_size
 var inputs = {"ui_right": Vector2.RIGHT,
 			"ui_left": Vector2.LEFT,
@@ -63,6 +64,15 @@ func _move(coords: Vector2) -> void:
 	var tween = create_tween()
 	tween.tween_property(self, "position", new_position, 0.05) 
 
+var _last_known_vector:= Vector2.ZERO
+func _get_joystick_direction(device := 0):
+	var x = Input.get_joy_axis(device, JOY_AXIS_LEFT_X)
+	var y = Input.get_joy_axis(device, JOY_AXIS_LEFT_Y)
+	var dir = Vector2(x,y)
+	if dir.length() < deadzone:
+		return _last_known_vector
+	_last_known_vector = dir
+	return dir
 
 ## by MikoStoro
 func get_relative_mouse_position():
@@ -75,7 +85,8 @@ var direction : int = 0
 @onready var base_rotation : int = self.rotation
 @onready var rotation_tween = create_tween()
 func _process(delta) -> void:
-	var new_direction = Direction_Tools.get_direction_index(get_relative_mouse_position().normalized())
+	#var new_direction = Direction_Tools.get_direction_index(get_relative_mouse_position().normalized())
+	var new_direction = Direction_Tools.get_direction_index(_get_joystick_direction().normalized())
 	if new_direction != direction:
 		rotate_to_direction(new_direction)
 		self.direction = new_direction
