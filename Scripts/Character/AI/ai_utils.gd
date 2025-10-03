d7d474f986e3289658a887a837d1885366694126
Mikołaj Storoniak <miko.storo@gmail.com>
2025-10-03 19:21:21 +0200

    ai works, still stupid (also, catastrophic bugs)

--- Scripts/Character/AI/ai_utils.gd
+++ Scripts/Character/AI/ai_utils.gd
@@ -1,8 +1,8 @@
 class_name AI_utils
 
-func get_nearest_player(my_pos : Vector2):
+static func get_nearest_player_dir(my_pos : Vector2):
 	var players : Array[Character] = GlobalComponents.character_manager.get_players()
 	var player : Character = players[0] 
-	var player_pos = player.get_spirit_position()
-	##TODO
+	var player_pos = player.get_spirit_position().get_vector2()
+	return Direction_Tools.get_direction_angle_v(player_pos - my_pos)
 	
--- Scripts/Character/AI/basic_ai_controller.gd
+++ Scripts/Character/AI/basic_ai_controller.gd
@@ -1,19 +1,27 @@
 extends CharacterController
 class_name BasicAIController
 
+var action : BaseAction = ShortDashAction.new()
+var action_direction : int = -1
+
 #to be overriden
 func _input_window_enter():
-	pass
+	action_direction = AI_utils.get_nearest_player_dir(self.spirit.coordinates.get_vector2())
 	
 #to be overriden
 func _input_window_leave():
-	pass
+	if action_direction > -1:
+		action.perform_action(self, spirit, action_direction)
 
 #to be overriden
 func _stop_action():
-	pass
+	action.end_action(self)
 
 #to be overriden
 func _on_beat():
 	pass
 	
+func _ready() -> void:
+	self.controls = PlayerControls.new()
+	self.controls.is_keyboard = false
+	super._ready()
