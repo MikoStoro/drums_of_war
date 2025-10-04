a98b59f9924dce6b3ada7640bf5f759fcf2f4841
Mikołaj Storoniak <miko.storo@gmail.com>
2025-10-04 21:47:41 +0200

    APPROACHING RAPIDLY

--- Scripts/Character/AI/ai_utils.gd
+++ Scripts/Character/AI/ai_utils.gd
@@ -4,5 +4,6 @@ static func get_nearest_player_dir(my_pos : Vector2):
 	var players : Array[Character] = GlobalComponents.character_manager.get_players()
 	var player : Character = players[0] 
 	var player_pos = player.get_spirit_position().get_vector2()
-	return Direction_Tools.get_direction_angle_v(player_pos - my_pos)
+	var diff = (player_pos - my_pos)
+	return Direction_Tools.get_direction_index_AI(diff)
 	
