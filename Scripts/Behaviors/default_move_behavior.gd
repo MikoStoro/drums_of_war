543847893644408d0501c7adad0a040241f0014e
Mikołaj Storoniak <miko.storo@gmail.com>
2025-10-06 20:40:40 +0200

    there is now a death event

--- Scripts/Behaviors/default_move_behavior.gd
+++ Scripts/Behaviors/default_move_behavior.gd
@@ -1,7 +1,2 @@
 class_name DefaultMoveBehavior
 extends EntityBehavior
-
-func collide(other: BoardEntity = null) -> BoardEvent:
-	return null
-func junction_collide(other: BoardEntity = null) -> BoardEvent:
-	return null
--- Scripts/Behaviors/ram_behavior.gd
+++ Scripts/Behaviors/ram_behavior.gd
@@ -3,11 +3,8 @@ class_name RamBehavior
 
 var ram_damage = 1
 
-func collide(other: BoardEntity = null) -> BoardEvent: ## to-do: make colliding entities able to interact
-	var event = BoardEvent.new(GlobalEnums.event_type.HIT, [other, null], [other.coordinates.get_vector2()])
-	event.add_data(other.hit_direct(ram_damage))
-	return event
-func junction_collide(other: BoardEntity = null) -> BoardEvent:
-	var event = BoardEvent.new(GlobalEnums.event_type.HIT, [other,null], [other.coordinates.get_vector2()])
-	event.add_data(other.hit_direct(ram_damage))
-	return event
+func collide(other: BoardEntity = null) -> Array[BoardEvent]: ## to-do: make colliding entities able to interact
+	return other.hit_direct(ram_damage)
+
+func junction_collide(other: BoardEntity = null) -> Array[BoardEvent]:
+	return other.hit_direct(ram_damage)
