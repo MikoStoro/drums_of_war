534450e88ae55835b91146e8c8a614bc5467c12d
Executed419 <lucker10@interia.pl>
2025-10-17 19:29:26 +0200

    choice for spawning player/AI

--- Scripts/UI/button_spawn.gd
+++ Scripts/UI/button_spawn.gd
@@ -1,8 +1,11 @@
 extends Button
 @export var x:TextEdit
 @export var y:TextEdit
-
+@export var is_ai:CheckBox
 func _on_button_down() -> void:
 	var z: Character_Manager = get_tree().get_first_node_in_group("CharacterManager")
-	z.add_npc(GlobalEnums.npc_type.charger, {"x" = int(x.text), "y" = int(y.text)})
+	var pos = {"x" = int(x.text), "y" = int(y.text)}
+	if is_ai.button_pressed:
+		z.add_npc(GlobalEnums.npc_type.charger, pos)
+	z.add_character(pos)
 	#z.add_character(y.text.to_int(),x.text.to_int())
